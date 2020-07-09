4700945f37452d6a24d7c77b84d2e60c5904c608
Dwi Prastowo <dprastowo@gmail.com>
2020-07-10 03:12:19 +0700

    deploy project baru react dgn axios

---
+++ precache-manifest.3ce78ab1b5306627930c92e565caf969.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e13b8dd80cc8498116a5362e3649a8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d95b669c607f629a7189",
+    "url": "/static/css/main.50c61b77.chunk.css"
+  },
+  {
+    "revision": "6374b44df704401b3581",
+    "url": "/static/js/2.b867d86b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b867d86b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d95b669c607f629a7189",
+    "url": "/static/js/main.1da0fb20.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
